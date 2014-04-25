d91f1e58f254f089c3394018324fcf519b90640f
eleomir <eleomir@gmail.com>
2014-04-25 11:55:57 +0200

    dodanie buttona z rezerwacją + małe zmiany na głównej

--- cont.php
+++ cont.php
@@ -14,6 +14,10 @@
 				<br><br><strong>Kontakt telefoniczny po godz. 15.00</strong>
 				<br><br>
 				Wizyty można umawiać od maja.
+				<br><br>
+				Możliwa jest także rezerwacja wizyty przez Internet. Aby umówić się na termin, proszę kliknąć przycisk poniżej:
+				<br><br>
+				<script type="text/javascript" src="http://nowehoryzonty.igabinet.pl/api/js/cmg.js" charset="utf-8"></script><script type="text/javascript" charset="utf-8">cmg_b("http://nowehoryzonty.igabinet.pl/api/?i=YTo5OntzOjE6ImwiO3M6MzoicG9sIjtzOjI6ImxsIjtpOjA7czoxOiJwIjtOO3M6MjoicGwiO2k6MDtzOjE6ImciO047czoyOiJnbCI7aTowO3M6MToicyI7TjtzOjI6InNsIjtpOjA7czoxOiJhIjtzOjE6InIiO30%3D_7bbaed9131d44e4e3a1e687f154b2c0bba68accfe43ed05b965265a5171a02ea", "pol", 0);</script>
 				
 			</p>	
 			
--- main.php
+++ main.php
@@ -1,7 +1,10 @@
 <p>
 	<div align = "center">
-	Witaj na stronie pracowni psychologicznej <strong><em>Nowe Horyzonty</em></strong>. Zachęcam do zapoznania się z formami oferowanej pomocy psychologicznej. Szczegóły znajdziesz w zakładce 
-	<a href="index.php?t=off">oferta</a>
+	Witaj na stronie Pracowni Psychologicznej <strong><em>Nowe Horyzonty</em></strong>. Zachęcam do zapoznania się z formami oferowanej pomocy psychologicznej. Szczegóły znajdziesz w zakładce 
+	<a href="index.php?t=off">oferta</a>.
+	<br><br>
+	Istnieje możliwość zarezerwowania terminu wizyty przez Internet - w tym celu zapraszam na zakładkę 
+	<a href="index.php?t=cont">kontakt</a>.
 	<br><br><br><br><br><br>
 	</div>
 	
